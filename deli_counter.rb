fe0c9cb16172944c92bdd84890a0508aee0a23d3
David Van Fleet <dvfleet413@gmail.com>
2020-01-22 22:50:22 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -18,7 +18,8 @@ def now_serving(array)
   if array.size == 0 
     puts "There is nobody waiting to be served!"
   else
-    
+    customer = array.shift
+    puts "Now serving #{customer}"
   end
 end
 
