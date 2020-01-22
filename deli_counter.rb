e940aa79d5b3dd35e627e9990d863e6fc896c301
David Van Fleet <dvfleet413@gmail.com>
2020-01-22 22:44:55 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -3,9 +3,8 @@ def line(array)
   if array.size == 0 
     puts "The line is currently empty."
   else 
-    print "The line is currently: "
-    array.each_with_index {|name, index| print "#{index + 1}. #{name} "}
-    return nil
+    puts "The line is currently: "
+    array.each_with_index {|name, index| puts "#{index + 1}. #{name} "}
   end
 end
 
